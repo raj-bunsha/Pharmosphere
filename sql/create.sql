bb39085bf15ccc514d825ab037debdfbaf4bab83
raj <rajbunsha@gmail.com>
2023-04-25 22:41:18 +0530

    Updated create

--- sql/create.sql
+++ sql/create.sql
@@ -1,19 +1,21 @@
 create database project;
 use project;
+CREATE TABLE Pharmacy(
+    id int PRIMARY KEY,
+    pharmacy_name varchar(20),
+    password varchar(30),
+    location varchar(100)
+);
 CREATE TABLE Medicine
 (
     id int PRIMARY KEY,
     medicine_name varchar(30),
     details varchar(100),
 );
-
 CREATE TABLE Substitute(
-    Medicine_id int,
-    Substitute_id int,
+    medicine_id int,
+    substitute_id int,
 );
-CREATE TABLE 
-    FOREIGN KEY(Medicine_id) REFERENCES Substitute(id)
-
 CREATE TABLE Customer
 (
     id int PRIMARY KEY,
@@ -22,26 +24,27 @@ CREATE TABLE Customer
 );
 CREATE TABLE Inventory(
     id int PRIMARY KEY,
-    pharma_id int,
+    pharmacy_id int,
     medicine_id int,
     quantity int,
     price int,
     expiry_date DATE,
-    manufacturer_id int,
     FOREIGN KEY(medicine_id) REFERENCES Medicine(id),
+    FOREIGN KEY(pharmacy_id) REFERENCES Pharmacy(id)
 );
 CREATE TABLE Orders(
     id int PRIMARY KEY,
     customer_id int,
-    medicine_id int,
+    pharmacy_id int,
     order_date DATE,
-    total_price int,
-    FOREIGN KEY(customer_id) REFERENCES Customer(id),
-    FOREIGN KEY(medicine_id) REFERENCES Inventory(id)
+    FOREIGN KEY(customer_id) REFERENCES Customer(id)
 );
-
 CREATE TABLE SellRecords(
     id int PRIMARY KEY,
     order_id int,
-    FOREIGN KEY(order_id) REFERENCES Orders(id)
+    medicine_id int,
+    price int,
+    quantity int,
+    FOREIGN KEY(order_id) REFERENCES Orders(id),
+
 );
